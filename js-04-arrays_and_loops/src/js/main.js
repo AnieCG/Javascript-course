4d8f768a52fe1d9c20a7f7481b2708c0ebf4c737
AnieCG <qfb.anacg@gmail.com>
2025-01-30 17:17:31 -0600

    ciclos ejercicios

--- js-04-arrays_and_loops/src/js/main.js
+++ js-04-arrays_and_loops/src/js/main.js
@@ -1,3 +1,4 @@
+
 const postres = [];
 
 // Agregar un postres al final del arreglo.
@@ -6,68 +7,57 @@ postres.push("Flan", "Gelatina");
 
 console.log(  postres ); // ["Pastel de Guayaba", "Flan", "Gelatina"]
 
-// Eliminar un postre del final del arreglo.
+// Eliminar un postre al final del arreglo.
 console.log(`Elemento eliminado con pop() ${postres.pop()}`); // Gelatina
 console.log(  postres ); // ["Pastel de Guayaba", "Flan"]
 
 // Agregar un postre al inicio del arreglo.
 postres.unshift("Tiramisú");
-
 console.log(  postres ); // ["Tiramisú", "Pastel de Guayaba", "Flan"]
-
 
 // Eliminar un postre al inicio del arreglo.
 console.log(`Elemento eliminado con shift() ${postres.shift()}`); // Tiramisú
-console.log(  postres ); // ["Pastel de Guayaba", "Flan"]
 
+console.log(  postres ); // ["Pastel de Guayaba", "Flan"]
 
 // Buscar un elemento dentro del arreglo usando indexOf().
 console.log(`Índice de "Flan": ${postres.indexOf("Flan")}`); // 1
 
-//Buscar el postre "Flan"
-//Si existe mostrar "El flan existe"
-//Si no existe mostrar "El flan no existe"
-//Para evaluar usar el operador ternario.
-//evaluación ? valor si verdadero : valor si falso
-
+// Buscar el postres "Flan" 
+// Si existe mostrar "El flan existe"
+// Si no existe mostrar "El flan no existe"
+// Para evaluar usar el operador ternario.     evaluación ? valor si verdadero : valor si falso
 postres.indexOf('Flan') != -1 ? console.log('El flan existe'): console.log('El flan no existe');
-console.log(postres.indexOf("Flan") === -1 ? "El flan existe" : "El flan no existe");
+const msjExistencia = postres.indexOf("Flan") === -1 ? "El flan no existe" : "El flan existe";
+console.log(msjExistencia);
 
-
-// =================================================
-let pasteles = ["Pastel de Chocolate", "Pastel de Zanahoria", "Pastel de Limón"];
-let eliminarPastel = ( array, indice ) => {
-    const pastelEliminado = array.splice(indice, 1);
-    console.log( array ); // ["Pastel de Chocolate", "Pastel de Limón"]
-    return pastelEliminado;
-}
-eliminarPastel( pasteles, 1 ); 
-console.log( pasteles ); // ["Pastel de Chocolate", "Pastel de Limón"]
+console.log( postres.indexOf("Flan") != -1 ? "El flan existe" : "El flan no existe" );
 
 // =================================================
 // El método slice crea una copia superficial de un fragmento de un array y devuelve un nuevo array.
 
-let pasteles = ["Pastel de Chocolate", "Pastel de Zanahoria", "Pastel de Limón"];
-let eliminarPastel = ( array, indice ) => {
+
+const pasteles = ["Pastel de Chocolate", "Pastel de Zanahoria", "Pastel de Limón"];
+
+const eliminarPastel = ( array, indice ) => {
     const copyArray = array.slice();    
     const pastelEliminado = copyArray.splice(indice, 1);
     console.log( copyArray ); // ["Pastel de Chocolate", "Pastel de Limón"]
     return pastelEliminado;
 }
+
 eliminarPastel( pasteles, 1 ); 
 console.log( pasteles ); // ["Pastel de Chocolate", "Pastel de Zanahoria", "Pastel de Limón"]
+
 // =============== Declarar un arreglo ===============
 const myArray = []; // []
 const myArray2 = new Array(); // []
+
 const myArray3 = [5]; // [5]
 const myArray4 = new Array(5); // [undefined, undefined, undefined, undefined, undefined]
+
 const myArray5 = [5,6]; // [5,6]
 const myArray6 = new Array(5,6); // [5,6]
-
-
-
-
-// Para detener un ciclo infinito ESC + SHIFT + Control ...eliminarPastel.apply.call.bind. localizar el navegador, dar click derecho y finalizar tarea 
 
 // ============ Iterar un  arreglo con for loop ============
 /*
@@ -75,6 +65,7 @@ const myArray6 = new Array(5,6); // [5,6]
     for (let i = 0; i < array.length; i++) {
         console.log(array[i]);
     }
+
 */
 const nombres = ["Juan", "Pedro", "María", "Ana"];
 for ( let index = 0; index < nombres.length; index++ ){
@@ -82,15 +73,15 @@ for ( let index = 0; index < nombres.length; index++ ){
 }
 console.log( "Me muestro en consola, muchas veces?" ); // Se muestra una vez en la consola
 
-
-
-
 // ============ Iterar un  arreglo y mostrar los elementos como una lista ============
+
 const gelatinas = ["Fresa", "Limon", "Naranja", "Uva"];
 const gelatinasLista = [];
+
 for ( let index = 0; index < gelatinas.length; index++ ) {
     gelatinasLista.push(`<li> ${ gelatinas[index] } </li>`);
 }
+
 console.log( gelatinasLista ); 
 /* 
   [
@@ -100,7 +91,79 @@ console.log( gelatinasLista );
    "<li> Uva </li>"
    ]
 */
-
-
 const refGeletinasLista = document.getElementById("gelatina-lista");
 console.log(refGeletinasLista);
+refGeletinasLista.innerHTML = gelatinasLista.join(""); // "<li></li><></><></><></>"
+
+// [1,2,3,4] -> string -> "1,2,3,4"
+// [<li></li>,<></>,<></>,<></>] -> string -> "<li></li>,<></>,<></>,<></>"
+
+
+// Simplificaciones en la iteración de un arreglo
+// ----------------------- Uso de for... of ------------------
+/* Ejecuta una sentencia por cada elemento de un objeto iterable(array, colección, string).
+Sintaxis:
+    for (const iterator of object) {
+    
+    }
+*/
+
+const colores = ["Rojo", "Azul", "Verde", "Amarillo"];
+
+for (let i = 0; i < colores.length; i++) {
+    const element = colores[i];
+    console.log(element);    
+}
+
+for ( const color of colores ) {
+    console.log( color );
+}
+
+// ---------------------- Ejercicio con for of ---------------------
+/**
+ *  Del siguiente arreglo de cantantes, mostrar en el DOM, el listado como un order list.
+ *  const cantantes = ["Juan Gabriel", "José José", "Rocío Dúrcal", "Ana Gabriel"];
+ *  - Usar for of
+ *  - De preferencia usar una función 
+ */
+
+const cantantes = ["Juan Gabriel", "José José", "Rocío Dúrcal", "Ana Gabriel"];
+const refListaCantantes = document.getElementById("cantantes-lista");
+const generarListaCantantes = (cantantesArray) => {
+    const cantantesLista = [];
+    for (const cantante of cantantesArray) {
+        cantantesLista.push(<li>${cantante}</li>);
+    }
+    return cantantesLista.join("");
+}
+const insertarListaDeCantantesAlDOM = (lista, refDom)=> refDom.innerHTML = lista;
+const listItemsOfCantantes = generarListaCantantes(cantantes);
+insertarListaDeCantantesAlDOM( listItemsOfCantantes, refListaCantantes);
+
+let iteracion = 0;
+for ( ;   ;  ){
+    console.log( Núm de iteración: ${iteracion} );
+    iteracion++;
+}
+
+
+// ------------------- Uso de break en ciclos ----------------------------
+// break detiene la ejecución de la iteración en curso y termina el ciclo.
+let iteracion = 0;
+for ( ;   ;  ){
+    console.log( Núm de iteración: ${iteracion} ); // 0, 1, 2, 3, 4
+    iteracion++;
+    if ( iteracion === 5 ){
+        break;
+    }
+}
+
+// ------------------- Uso de break y label en ciclos anidados ----------------------------
+multiplicando:
+for (let i = 1; i <= 7; i++ ){
+    multiplicador:
+    for (let j = 1; j <= 10 ; j++){        
+        console.log(${i} x ${j} = ${i * j});             
+        if( i >= 4 ) break multiplicando;
+    }
+}
