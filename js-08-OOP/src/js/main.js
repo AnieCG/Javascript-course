7906e13349fd201657c427641769c0ee8c7653c5
MauSalazar12 <mauriciosalazar988@gmail.com>
2025-02-11 10:05:04 -0600

    feat Clase de objetos

--- js-08-OOP/src/js/main.js
+++ js-08-OOP/src/js/main.js
@@ -4,3 +4,22 @@ import { insertMainFooter } from "/src/modules/footer/footer.js";
 insertMainHeader();
 insertMainFooter();
 
+console.log( amdRadeonRx );
+// Podemos acceder a los atributos usando el operador objeto.atributo (access and call), n.precedence: 17
+// podemos acceder usando objeto[nombreAtributo]
+
+// Accediendo al modelo
+console.log( amdRadeonRx.model );
+// console.log( amdRadeonRx["model"] );Esta forma es dinámica, es la ventaja con respecto a la otra opción.
+
+let attribute = "model"
+console.log( amdRadeonRx[attribute] );
+
+
+attribute = "price";
+console.log( amdRadeonRx[attribute] ); // 10699
+// Acceder al atributo title e imprimir en consola, usando los dos métodos para aceder
+
+attribute = "title"
+console.log(amdRadeonRx.title);
+console.log(amdRadeonRx[attribute]);
