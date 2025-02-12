6478c9bc7d0658688cd6b5f585be42b4f5c2477b
AnieCG <qfb.anacg@gmail.com>
2025-02-12 14:09:34 -0600

    feat: se importan js de boodstrap

--- js-09-npm/src/pages/about-us/about.js
+++ js-09-npm/src/pages/about-us/about.js
@@ -1,5 +1,7 @@
 import { insertMainHeader } from "/src/modules/header/header.js";
 import { insertMainFooter } from "/src/modules/footer/footer.js";
+// Import all of Bootstrap's JS
+import * as bootstrap from 'bootstrap'
 
 insertMainHeader();
 insertMainFooter();
